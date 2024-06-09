e6ad430718723478061479033889d3795945e8c4
mjkushman <mjkushman@gmail.com>
2024-06-09 00:23:05 -0700

    added orgUser model and routes. Have not added orgUser relations. I'll switch to sequelize before doing that I think

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const postRoutes = require("./routes/postRoutes");
 const { verifyJWT } = require("./middleware/authorizations");
 const authRoutes = require("./routes/authRoutes");
 const usersRoutes = require("./routes/userRoutes");
+const agentRoutes = require("./agents/agentRoutes");
+const orgRoutes = require("./orgs/orgRoutes");
+const orgUserRoutes = require("./orgUser/orgUserRoutes")
 const morgan = require("morgan");
+
 
 const app = express();
 
@@ -20,6 +24,9 @@ app.use(morgan("tiny"));
 app.use(verifyJWT); // stores decoded token on res.locals.user, if one is provided
 
 app.use("/auth", authRoutes);
+app.use("/agents", agentRoutes);
+app.use("/orgs", orgRoutes);
+app.use("/orgusers",orgUserRoutes);
 
 app.use("/users", usersRoutes);
 app.use("/posts", postRoutes);
