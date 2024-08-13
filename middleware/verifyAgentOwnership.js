ec339633b06c227a9d4d98ac8512aa8d90107909
mjkushman <mjkushman@gmail.com>
2024-08-13 12:21:31 -0700

    added middleware to verify that a valid agentId is in req body

---
+++ middleware/verifyAgentOwnership.js
@@ -0,0 +1,21 @@
+// Verifies that an agent in a request body belongs to the account making the request.
+
+const { UnauthorizedError } = require("../utilities/expressError");
+
+module.exports = {
+  async verifyAgentOwnership(req, res, next) {
+    const { account } = req;
+    const { agentId } = req.body;
+
+    let ownedAgentIds = account.agents.map((agent) => agent.agentId);
+    try {
+      console.log(`owned Ids: ${ownedAgentIds}`);
+      if (ownedAgentIds.includes(agentId)) return next();
+      throw new UnauthorizedError(
+        "A valid agentId owned by your account must be provided in the request body."
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+};
